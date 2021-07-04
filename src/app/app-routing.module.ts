3f614a76cb84f9353b9126d7a912b82b8aafcbdf
Armindo Malafaia Neto <malafaia.armindo@hotmail.com>
2021-07-04 19:13:51 -0400

    Added forms to register/list profiles, and inserted providers, and services
    Created the insert and list form as well as the services and providers files
    
    We include the necessary dependencies, also putting them in the app. module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'profiles',
+    loadChildren: () => import('./pages/profiles/profiles.module').then( m => m.ProfilesPageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'profiles',
     pathMatch: 'full'
+  },
+  {
+    path: 'profiles/:id',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,30 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
+import { SQLite } from '@ionic-native/sqlite/ngx';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { SummaryService } from './providers/summary.service';
+import { ProfileService } from './providers/profile.service';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot({ mode: 'ios' }),
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SQLite,
+    SQLitePorter,
+    SummaryService,
+    ProfileService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
